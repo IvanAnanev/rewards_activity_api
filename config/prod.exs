9510b60d29023a711f7a992a65ecf79bf8a35e5d
Ivan Ananev <ivcheg@gmail.com>
2019-04-29 17:15:11 +0300

    add bot in prod

--- config/prod.exs
+++ config/prod.exs
@@ -14,5 +14,15 @@ config :activity_api, ActivityApi.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
+config :activity_api,
+  bots: %{
+    "flatstack" => %{
+      name: System.get_env("FLATSTACK_ACTIVITY_BOT"),
+      password: System.get_env("FLATSTACK_ACTIVITY_BOT_PASSWORD"),
+      message: "+:points to @:user_name #be-curious-never-stop-learning for :for_goal"
+    }
+  }
 
+config :activity_api,
+  rewards_url: System.get_env("REWARDS_URL")
 
